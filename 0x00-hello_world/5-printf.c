2c4d84bcb286335b387a3988b4e01758d59a5258
Freshi <wachirajob@gmail.com>
2023-02-17 03:00:58 +0300

    Added 5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-
 /**
- *
  * main - print usingg printf
+ *
+ * Description: Print to the stdout using printf
  *
  * Return: 0
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
