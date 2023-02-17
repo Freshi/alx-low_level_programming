258d9151a23fc3d843e3f7950fd8e4f9edfa8239
Freshi <wachirajob@gmail.com>
2023-02-17 03:24:07 +0300

    Added 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Start of the program
+ *
+ * Description: print type sizes by based on system.
+ * Return: 0
+ */
+
+int main(void)
+{
+	c = sizeof(char);
+	i = sizeof(int);
+	li = sizeof(long int);
+	lli = sizeof(long long int);
+	f = sizeof(float);
+
+	printf("Size of a char: %lu bytes(s)\n", c);
+	printf("Size of a char: %lu bytes(s)\n", c);
+	printf("Size of a char: %lu bytes(s)\n", c);
+	printf("Size of a char: %lu bytes(s)\n", c);
+	printf("Size of a char: %lu bytes(s)\n", c);
+
+	return (0);
+}
