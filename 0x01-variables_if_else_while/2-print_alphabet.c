df4746e07c51a683dfeae530a0067f4767bb3330
Freshi <wachirajob@gmail.com>
2023-02-18 05:27:59 +0300

    Prints alphabet in upper

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio>
+
+/**
+ * main - Printns alphabet
+ *
+ * Description: prints alphabet in upper case
+ * Return: 0
+ */
+
+int main(void)
+{
+	for (int x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(x);
+	}
+
+	putchar("\n);
+
+	return (0);
+}
