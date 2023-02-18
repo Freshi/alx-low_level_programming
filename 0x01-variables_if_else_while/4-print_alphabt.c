9f2f565261bb3d9d42b6520cc76908be0b890755
Freshi <wachirajob@gmail.com>
2023-02-18 05:58:56 +0300

    Added 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Printns alphabet
+ *
+ * Description: prints alphabet in upper case
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		if (x != 'e' || x != 'q')
+		{
+			putchar(tolower(x));
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
