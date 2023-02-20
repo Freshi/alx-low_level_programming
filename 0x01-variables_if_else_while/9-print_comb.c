b2d9b7337b072feaa623a2ca28e54cedf10d9f17
Freshi <wachirajob@gmail.com>
2023-02-20 16:32:53 +0300

    Patience and Persistence

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,21 +4,20 @@
  * main - Patience and persistence
  *
  * Description: Print all possible combinations of numbers
- * Result: 0
+ * Return: 0
  */
 
 int main(void)
 {
 	int i;
-	int j;
 
 	for (i = 0; i < 10; i++)
 	{
 		putchar(48 + i);
-		for (j = 0; j < 10; j++)
+		if (i < 9)
 		{
-			putchar(48 + j);
 			putchar(32);
+			putchar(44);
 		}
 	}
 
