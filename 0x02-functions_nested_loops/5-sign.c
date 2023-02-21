652ba51b2e586a39970caf6b8a9889f2c7a2e8ef
Freshi <wachirajob@gmail.com>
2023-02-21 22:56:37 +0300

    Added 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign of a number
+ * @n: number whose sign is checked
+ *
+ * Description: checks the sign of a given number and prints it out to stdout
+ * Return: 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (0);
+	}
+	else if (n == 0)
+	{
+		_purchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
