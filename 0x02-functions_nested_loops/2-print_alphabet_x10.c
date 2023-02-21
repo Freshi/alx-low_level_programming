ce3987cf4301ed6eb6e53f82ab29b4b1fb75a500
Freshi <wachirajob@gmail.com>
2023-02-21 20:55:45 +0300

    Added 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lines of alphabet
+ *
+ * Description: Prints ten lines of lowercase alphabet
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		int j;
+
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+
+		i++;
+	}
+}
