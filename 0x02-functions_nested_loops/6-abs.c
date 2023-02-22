3917414520769bf9f04d4286610fa401e06eba48
Freshi <wachirajob@gmail.com>
2023-02-22 16:17:42 +0300

    Added 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ * _abs - Returns absolute value of number
+ * @i: number to convert to absolute
+ *
+ * Description: Returns the absolute value of a given int
+ * Return: 0;
+ */
+
+int _abs(int i)
+{
+	if (i < 0)
+	{
+	return (-i);
+	}
+	else
+	{
+	return (i);
+	}
+}
