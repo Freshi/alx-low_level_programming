457714c2c87e494162ebc5686f36d8ec0ba44990
Freshi <wachirajob@gmail.com>
2023-02-22 20:34:00 +0300

    8. Fed

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints time
+ *
+ * Description: Prints mins from Midnight to a min toe midnight
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	int h;
+
+	for (h = 0; h < 24; h++)
+	{
+		int m;
+
+		for (m = 0; m < 60; min++)
+		{
+			_putchar(h);
+			_putchar(':');
+			_putchar(m);
+		}
+	}
+}
