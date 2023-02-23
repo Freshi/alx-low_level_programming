8010592b1f8d67b37c0732dee6b5c52c6c64fc02
Freshi <wachirajob@gmail.com>
2023-02-23 05:47:34 +0300

    Modified 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ void print_to_98(int n)
 	{
 		while (n >= 98)
 		{
-			_putchar(n);
+			_putchar('0' + n);
 			_putchar(',');
 			_putchar(' ');
 			n++;
@@ -24,7 +24,7 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
-			_putchar(n);
+			_putchar('0' + n);
 			_putchar(',');
 			_putchar(' ');
 			n++;
