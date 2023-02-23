8576a562a2223fff387e5705c5bd03c136a30e83
Freshi <wachirajob@gmail.com>
2023-02-23 19:45:39 +0300

    Added 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
