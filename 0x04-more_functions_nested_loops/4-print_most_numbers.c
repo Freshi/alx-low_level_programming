62907bc369e0880e82205847ff8dd598e26fbd32
Freshi <wachirajob@gmail.com>
2023-02-23 21:24:21 +0300

    Added 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints nums skipping 2 and 4
+ * Return: 0
+ */
+
+void print_most_numbers(void);
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 4 || i == 2)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(48 + i);
+		}
+	}
+	_putchar('\n');
+}
