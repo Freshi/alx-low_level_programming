a2a7d9406aac7c1125c708de8312ac7754021aab
Freshi <wachirajob@gmail.com>
2023-02-23 05:34:59 +0300

    Added 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints numbers
+ * @n: number to start
+ *
+ * Description: Prints starting with i to 98
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			_putchar(n);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else
+	{
+		while (n <= 98)
+		{
+			_putchar(n);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
