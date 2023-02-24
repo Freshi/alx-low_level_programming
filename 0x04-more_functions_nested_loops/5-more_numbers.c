7ff39ffe5a1b7063b5203b56ba9da9ab6e8abfbb
Freshi <wachirajob@gmail.com>
2023-02-24 20:55:39 +0300

    Modified the if statement

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,9 +12,17 @@ void more_numbers(void)
 	for (i = 0; i < 10; i++)
 	{
 		int j = 0;
-		while (j < 14)
+
+		while (j < 15)
 		{
-			_putchar(i/10 + '0');
+			if (j / 10 != 0)
+			{
+				_putchar((j / 10) + '0');
+			}
+
+			_putchar((j % 10) + '0');
+
+			j++;
 		}
 
 		_putchar('\n');
