7d12053fe2291a8f300cc1248aee29fee3510255
root <root@Job>
2024-01-12 18:03:22 +0300

    Added 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - prints out a string
+ * @str: string to print to stdout
+ *
+ * Description - prints out a string taken as an arg
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	for (; *str != 0; i++)
+	{
+		_putchar(*str);
+	}
+
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,4 +5,6 @@
 void reset_to_98(int *);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
+int _putchar(char c);
 #endif
