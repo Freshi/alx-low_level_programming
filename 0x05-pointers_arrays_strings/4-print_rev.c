ec1543d4ac511fff2dd11d7dd611d3c5b6a7a098
root <root@Job>
2024-01-13 08:59:30 +0300

    Added _strlen to 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-  * print_rev - prints string in reverse
-  * @s: string to print in reverse
-  *
-  * Description - prints string in reverse
-  * Return: 0
-  */
+ * _strlen - gets the length of a string
+ * @s: string to get length of
+ *
+ * Description - returns the length of string pointed to
+ * Return: 0
+ */
 
 int _strlen(char *s)
 {
@@ -21,9 +21,17 @@ int _strlen(char *s)
 	return (len);
 }
 
+/**
+  *print_rev - prints string in reverse
+  *@s: string to print in reverse
+  *
+  *Description - prints string in reverse
+  *Return: 0
+  */
+
 void print_rev(char *s)
 {
-	int i = _strlen(s);
+	int i = _strlen(s) - 1;
 
 	for (; i > -1; i--)
 	{
