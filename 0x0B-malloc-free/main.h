a90f629559c80a8a84a0c29c3a4117fe7bda548d
Wachirajob <wachirajob@gmail.com>
2024-01-17 00:17:48 +0300

    Delete 0x0B-malloc-free directory
    Wrongly named. Replaced with 0x0b-malloc_free

--- 0x0B-malloc-free/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef main_h
-#define main_h
-
-#include <stdio.h>
-int _putchar(char c);
-int _strlen_recursion(char *s);
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-#endif
